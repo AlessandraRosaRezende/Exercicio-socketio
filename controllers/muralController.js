b7fa83a4f50c3aba75581020e70c52b28217d010
Mariana Mohr <marianamohr@gmail.com>
2021-10-25 18:28:34 -0300

    parte Mari

--- controllers/muralController.js
+++ controllers/muralController.js
@@ -1,16 +1,31 @@
 const muralModel = require('../models/muralModels');
+const socketPort = 4555;
+
+const socketIoServer = require('http').createServer();
+
+const io = require('socket.io')(socketIoServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  }
+});
 
 const getAll = async (_req, res) => {
   const result = await muralModel.getAll();
-  return res.status(200).json(result);
+  console.log(result);
+  return res.status(200).render('mural', result );
+  //return res.status(200).json(result);
 };
 
 const createPost = async (req, res) => {
-  const result = await muralModel.createPost(req.body)
+  const { nome, post } = req.body;
+  const result = await muralModel.createPost(req.body);
+  io.emit('mural', result);
   return res.status(201).json(result);
 };
 
-module.exports = { 
-  getAll, 
+module.exports = {
+  getAll,
   createPost,
 }
+socketIoServer.listen(socketPort);
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }),
+);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
 
 const mural = require('./routers/muralRouter');
